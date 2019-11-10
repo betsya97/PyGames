0c5255e5c167b6a5f9c7cd6da558de6fef23311e
Betsy Avila <bavila24@ucmerced.edu>
2019-11-09 22:44:08 -0800

    Left paddle size fixed

--- Pong1.py
+++ Pong1.py
@@ -11,15 +11,15 @@ wn.setup(width = 800, height = 600)
 wn.tracer(0)
 
 #paddle A
-
-#paddle B
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("white") 
+paddle_a.color("white")  
+paddle_a.shapesize(stretch_wid = 5, stretch_len = 1)
 paddle_a.penup()
 paddle_a.goto(-350,0) 
  
+#paddle B
 
 #ball
 paddle_b = turtle.Turtle()
