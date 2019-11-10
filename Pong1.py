6fa72c1cf6453cd5c1cb1924bb1b8efec9c1bef3
Betsy Avila <bavila24@ucmerced.edu>
2019-11-09 23:22:52 -0800

    keyboard bindng to control the right paddle

--- Pong1.py
+++ Pong1.py
@@ -44,12 +44,25 @@ def paddle_a_up():
 def paddle_a_down(): 
     y = paddle_a.ycor() #returns the y coordinate downwards
     y -= 20 
-    paddle_a.sety(y) 
-    
+    paddle_a.sety(y)  
+
+def paddle_b_up(): 
+    y = paddle_b.ycor() #returns the y coordinate upwards
+    y += 20 
+    paddle_b.sety(y)  
+
+def paddle_b_down(): 
+    y = paddle_b.ycor() #returns the y coordinate downwards
+    y -= 20 
+    paddle_b.sety(y) 
+
 #keyboard binding 
 wn.listen()
 wn.onkeypress(paddle_a_up,"w") 
 wn.onkeypress(paddle_a_down,"s") 
+
+wn.onkeypress(paddle_b_down,"Up") 
+wn.onkeypress(paddle_b_down,"Down") 
 
 
 #Main Game Loop 
